5568ccab0827f414f7fa2dbd9131cc5f42208995
YunHojun <yhj020996@gmail.com>
2019-11-04 01:07:05 +0900

    Goods check game

---
+++ 닭/endgame/Assets/Scenes/TextScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    public Text text;
+    int score;
+    GameObject Manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Manager = GameObject.FindGameObjectWithTag("GameController");
+        score = 0;
+        text.text = "SCORE: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = Manager.GetComponent<CreaterScript>().score;
+        text.text = "SCORE: " + score;
+    }
+}
