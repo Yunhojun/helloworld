629e2d1c0054fa9fe51d99c0d440d8d0a83cf935
unknown <yhj020996@gmail.com>
2019-11-09 16:24:46 +0900

    Camera test update

--- unity/CameraTest/Assets/CameraScript.cs
+++ unity/CameraTest/Assets/CameraScript.cs
@@ -5,29 +5,72 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField]
-    float speed = 1f;
+    float speed = 5f;
+    [SerializeField]
+    float rspeed = 100f;
+    [SerializeField]
+    float jumpPower = 5f;
+
+    GameObject cam;
+    GameObject subC;
+
+    float rx;
+    float ry;
+    float h;
+    float v;
+
+    Rigidbody rigid;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigid = GetComponent<Rigidbody>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        subC = GameObject.FindGameObjectWithTag("SubCamera");
         Cursor.lockState = CursorLockMode.Locked;
+        rx = 0;
+        ry = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (cam.GetComponent<Camera>().enabled == true)
+        {
+            move(h, v);
+            rotate();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        move(h, v);
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
 
-        float rx = Input.GetAxis("Mouse X");
-        float ry = Input.GetAxis("Mouse Y");
-        transform.rotation = Quaternion.Euler(ry, rx, 0);
+        rx += Input.GetAxis("Mouse X") * rspeed * Time.deltaTime ;
+        ry += Input.GetAxis("Mouse Y") * rspeed * Time.deltaTime ;
+        
     }
 
     private void move(float h, float v)
     {
         Vector3 dir = new Vector3(h, 0, v);      
         transform.Translate(dir.normalized * speed * Time.deltaTime);
+        cam.transform.position = transform.position + Vector3.up * 1;
+        subC.transform.position = transform.position + Vector3.up * 15;
+    }
+
+    private void rotate()
+    {    
+        cam.transform.rotation = Quaternion.Euler(-ry, rx, 0);
+        transform.rotation = Quaternion.Euler(0, rx, 0);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
     }
 }
---
+++ unity/CameraTest/Assets/Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    GameObject mainc;
+    GameObject subc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainc = GameObject.FindGameObjectWithTag("MainCamera");
+        subc = GameObject.FindGameObjectWithTag("SubCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            CameraSwitch();
+        }
+    }
+
+    void CameraSwitch()
+    {
+        if(mainc.GetComponent<Camera>().enabled == true)
+        {
+            mainc.GetComponent<Camera>().enabled = false;
+            mainc.GetComponent<AudioListener>().enabled = false;
+            subc.GetComponent<Camera>().enabled = true;
+            subc.GetComponent<AudioListener>().enabled = true;
+        }
+        else
+        {
+            mainc.GetComponent<Camera>().enabled = true;
+            mainc.GetComponent<AudioListener>().enabled = true;
+            subc.GetComponent<Camera>().enabled = false;
+            subc.GetComponent<AudioListener>().enabled = false;
+        }
+    }
+}
