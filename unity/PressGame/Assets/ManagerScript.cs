67cdec53c909eccbd3a479473257e1cf3f49a9b2
unknown <yhj020996@gmail.com>
2019-11-09 19:08:54 +0900

    PressGame

---
+++ unity/PressGame/Assets/ManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obj;
+    public int score = 0;
+    public bool lever;
+    float cycle = 2f;
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        lever = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(score);
+        time += Time.deltaTime;
+        if(time >= cycle)
+        {
+            ins();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!lever)
+        {
+            lever = true;
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+    void ins()
+    {
+        Instantiate<GameObject>(obj, new Vector3(-10f, -1.5f, 0f), Quaternion.identity);      
+        time = 0;
+    }
+}
---
+++ unity/PressGame/Assets/PressScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressScript : MonoBehaviour
+{
+    GameObject lever;
+    Rigidbody rigid;
+    [SerializeField]
+    float speed = 10f;
+    [SerializeField]
+    float forcef = 100f;
+    bool up;
+    [SerializeField]
+    GameObject obj2;
+    GameObject manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameController");
+        up = false;
+        rigid = gameObject.GetComponent<Rigidbody>();
+        lever = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lever.GetComponent<ManagerScript>().lever && !up)
+        {
+            downPress();
+        }
+        if (up)
+        {
+            upPress();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "belt")
+        {
+            up = true;
+        }
+       
+        
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "obj")
+        {
+            Destroy(other.gameObject,0f);
+            Instantiate<GameObject>(obj2, other.transform.position + Vector3.down * 0.5f, Quaternion.identity);
+            obj2.GetComponent<objScript>().Pressed = true;
+            manager.GetComponent<ManagerScript>().score += 10;
+        }
+    }
+
+    void upPress()
+    {
+        if (transform.position.y < 4.5f)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            lever.GetComponent<ManagerScript>().lever = false;
+            lever.GetComponent<MeshRenderer>().material.color = Color.green;
+            up = false;
+            return;
+        }
+    }
+
+    void downPress()
+    {
+        rigid.AddForce(Vector3.down * forcef * Time.deltaTime,ForceMode.Impulse);
+    }
+}
