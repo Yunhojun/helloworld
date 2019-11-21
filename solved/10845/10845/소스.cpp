bf2650220ea0323615cdd027fb28ec567c9ec748
YunHojun <yhj020996@gmail.com>
2019-11-21 11:08:48 +0900

    Solved 10845

--- solved/10845/10845/소스.cpp
+++ solved/10845/10845/소스.cpp
@@ -4,5 +4,56 @@
 using namespace std;
 
 int main() {
-	int num;
+	vector<int> num;
+	int front = 0;
+	int back = 0;
+
+	string input;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		if (input == "push") {
+			int m;
+			cin >> m;
+			num.push_back(m);
+			back++;
+		}
+		else if (input == "pop") {
+			if (front == back) {
+				cout << -1 << endl;
+			}
+			else {
+				cout << num[front] << endl;
+				front++;
+			}
+		}
+		else if (input == "size") {
+			cout << (back - front) << endl;
+		}
+		else if (input == "empty") {
+			if (front == back) {
+				cout << 1 << endl;
+			}
+			else {
+				cout << 0 << endl;
+			}
+		}
+		else if (input == "front") {
+			if (front == back) {
+				cout << -1 << endl;
+			}
+			else {
+				cout << num[front] << endl;
+			}
+		}
+		else if (input == "back") {
+			if (front == back) {
+				cout << -1 << endl;
+			}
+			else {
+				cout << num[back - 1] << endl;
+			}
+		}
+	}
 }
